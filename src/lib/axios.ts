a4a286b5e8c11ddb3b61b3ab5b80fa2ebd6feec9
SuporteCDL <suportecdlanapolis@gmail.com>
2026-02-11 17:33:44 -0300

    Finish

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://192.168.2.106:5555',
+  baseURL: '/api',
   timeout: 99000,
   headers: {
     'Content-Type': 'application/json'
